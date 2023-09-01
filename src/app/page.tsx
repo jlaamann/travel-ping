32b02face301bd019d28a29eb8e909d509b7826d
Johannes Laamann <jelaamann@gmail.com>
2023-09-01 17:03:44 +0300

    Show map full page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import PhotoMap from "./components/photo-map";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between">
       <Head>
         <link
           href="https://api.mapbox.com/mapbox-gl-js/v1.10.1/mapbox-gl.css"
